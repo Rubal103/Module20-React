f2404554fa12a1c1b0a796002996ac671113207e
Rubal <rubal103@gmail.com>
2024-06-03 14:53:04 -0400

    added about me information

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h2>About</h2>
+     
+      <p>
+        Hi, my name is Rubal Grewal. I migrated to Canada 4 years ago. My eductaion background is in Accounting from India. I have Masters degree in Commerce and Business Administration. After finishing my studies, I worked for a Government Organisation for 6 years. Desire to get better opportunities drove me to come to Canada. I have been working as an Accouting Associate for 2 years now. My filed of interest is very diverse and I love to learn new things. I have always been interested in coding and wanted to learn it. After coming to Canada, I got this opportunity to learn Full Stack Development from Univeristy of Toronto. 
+      </p>
+      <p>
+        I am a detail-oriented and organised individual. I have used my problem solving skills and knack to learn new technology in every position I have worked before. Over the span of my career, I have learnt different accounting realted technologies. Now, after graduating in Full Stack Development, I have gained knowledge about all the trending technologies that are required one to be a good developer. I have worked with different teams on three projects as a part of my course. I have used my communication skills to present our work to our teachers and other teams. Now I want to offer my skills to the market and showcase my projects and different assignments that I have worked on. 
+      </p>
+    </div>
+  );
+}
